942d1f9c3622bee334cef6c705da39389dafb3da
belbecsa <belbecsa@sheridancollege.ca>
2020-01-23 15:08:06 -0500

    added fetch, pull, checkout comments

--- src/gitreview/GitReview.java
+++ src/gitreview/GitReview.java
@@ -8,10 +8,12 @@
         
 */
 
-// add moves from working tree to index stage
-// commit moves from index stage to local rep (or working tree to local rep)
+// add: moves from working tree to index stage
+// commit: moves from index stage to local rep (or working tree to local rep)
 // push: right click on project>git>remote>push>chose repository link
-
+// fetch: gets remote copy to update your local copy (doesn't touch working tree
+// pull: gets remote copy to update everything (local/wkg tree)
+// checkout: right click on remote branch - checkout revision>checkout as new branch
 
 package gitreview;
 

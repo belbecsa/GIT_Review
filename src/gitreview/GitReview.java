d18e11ff4f2f1165d903079d06e512341b21ed59
belbecsa <belbecsa@sheridancollege.ca>
2020-01-23 14:18:36 -0500

    First commit

---
+++ src/gitreview/GitReview.java
@@ -0,0 +1,27 @@
+/*
+        Name:  Sam Belbeck
+        Assignment:  
+        Program: 
+        Date:  
+    
+        Description:
+        
+*/
+
+package gitreview;
+
+/**
+ *
+ * @author S.B.
+ */
+public class GitReview {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  
+        System.out.println("Simple test.");
+    }
+
+}
